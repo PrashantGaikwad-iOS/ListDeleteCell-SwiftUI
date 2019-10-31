d0a8aad518bb7d07fe02abffba5385b9790a0291
Prashant Gaikwad <prashant.gaikwad@leftrightmind.com>
2019-10-31 18:35:23 +0530

    Initial Commit

---
+++ ListDeleteCell/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  ListDeleteCell
+//
+//  Created by Saif on 31/10/19.
+//  Copyright © 2019 LeftRightMind. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers,id:\.self) {
+                        Text("\($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number") {
+                    self.numbers.append(self.currentNumber)
+                    self.currentNumber += 1
+                }
+            }
+            .navigationBarItems(leading: EditButton())
+            .navigationBarTitle(Text("List - Delete"))
+        }
+    }
+    
+    func removeRows(at offset: IndexSet) {
+        numbers.remove(atOffsets: offset)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
